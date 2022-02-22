b812458e94fdc212e9a994eb3c635f590e9e3e5b
Agus Acevedo <agus.acevedo.100@gmail.com>
2022-02-22 20:35:01 -0300

    Firts steps, NavBar test

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+ return(
+        <header>
+            <ul>
+                <li>Termeras</li>
+                <li>Mates</li>
+                <li>Bombillas</li>
+                <li>Otros</li>
+            </ul>
+        </header>
+    )
+}
+
+export { NavBar }
